f76866d308f4490744a8702e9590bbb34257c387
Paulo Said <103071726+PauloSaid@users.noreply.github.com>
2024-02-14 12:59:02 -0300

    Add files via upload

---
+++ PauloSaid/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paulo_Said.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [Route("/Project")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Project/Clock")]
+        public IActionResult Clock() { return View(); }
+    }
+}
---
+++ PauloSaid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Paulo_Said.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
